d524a457acd9b04161a72e5f606b6e10544884fa
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-10-01 23:16:37 -0500

    Added Bootstrap

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,10 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> eventList = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> eventList = new ArrayList<>();
-        eventList.add("Code With Pride");
-        eventList.add("Strange Loop");
-        eventList.add("Apple WWDC");
-        eventList.add("SpringOne Platform");
         model.addAttribute("events", eventList);
         return "events/index";
     }
@@ -28,5 +26,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    // Lives at /events/create   ***This is ok because ^ is a Get request while v is a Post request***
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        eventList.add(eventName);
+        return "redirect:";
     }
 }
