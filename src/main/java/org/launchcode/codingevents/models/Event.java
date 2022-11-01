1ac79e2bf6f37f41a682d3e6e54805340103ed73
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-10-31 19:49:20 -0500

    One-to-One update

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Email;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -14,22 +16,18 @@ public class Event extends AbstractEntity{
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")  // Restricts size of the string
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
-    private String description;
-
-    @NotBlank(message = "Email is required.")
-    @Email(message = "Invalid Email. Try Again.") // Tells the program to expect email syntax (Ex. fake@email.com)
-    private String contactEmail;
-
     @ManyToOne
     @NotNull(message = "Category is required.")
     private EventCategory eventCategory;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
+
     public Event() {}
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
         this.eventCategory = eventCategory;
     }
 
@@ -37,17 +35,13 @@ public class Event extends AbstractEntity{
 
     public void setName(String name) { this.name = name; }
 
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
-
-    public String getContactEmail() { return contactEmail; }
-
-    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
-
     public EventCategory getEventCategory() { return eventCategory; }
 
     public void setEventCategory(EventCategory eventCategory) { this.eventCategory = eventCategory; }
+
+    public EventDetails getEventDetails() { return eventDetails; }
+
+    public void setEventDetails(EventDetails eventDetails) { this.eventDetails = eventDetails; }
 
     @Override
     public String toString() {
